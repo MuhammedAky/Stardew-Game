61c5ab4150948d499ce51b53ad6a1eb2fba48921
MuhammedAky <muhammedaky24@gmail.com>
2022-08-18 12:29:58 +0300

    soil tilling

--- code/level.py
+++ code/level.py
@@ -2,9 +2,11 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Tree
+from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
+from transition import Transition
+from soil import SoilLayer
 
 class Level:
 	def __init__(self):
@@ -16,9 +18,12 @@ class Level:
 		self.all_sprites = CameraGroup()
 		self.collision_sprites = pygame.sprite.Group()
 		self.tree_sprites = pygame.sprite.Group()
+		self.interaction_sprites = pygame.sprite.Group()
 
+		self.soil_layer = SoilLayer(self.all_sprites)
 		self.setup()
 		self.overlay = Overlay(self.player)
+		self.transition = Transition(self.reset, self.player)
 
 	def setup(self):
 		tmx_data = load_pygame('../data/map.tmx')
@@ -43,7 +48,12 @@ class Level:
 
 		# trees 
 		for obj in tmx_data.get_layer_by_name('Trees'):
-			Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+			Tree(
+				pos = (obj.x, obj.y), 
+				surf = obj.image, 
+				groups = [self.all_sprites, self.collision_sprites, self.tree_sprites], 
+				name = obj.name,
+				player_add = self.player_add)
 
 		# wildflowers 
 		for obj in tmx_data.get_layer_by_name('Decoration'):
@@ -60,12 +70,31 @@ class Level:
 					pos = (obj.x,obj.y), 
 					group = self.all_sprites, 
 					collision_sprites = self.collision_sprites,
-					tree_sprites = self.tree_sprites)
+					tree_sprites = self.tree_sprites,
+					interaction = self.interaction_sprites,
+					soil_layer = self.soil_layer)
+			
+			if obj.name == 'Bed':
+				Interaction((obj.x,obj.y), (obj.width,obj.height), self.interaction_sprites, obj.name)
+
+
 		Generic(
 			pos = (0,0),
 			surf = pygame.image.load('../graphics/world/ground.png').convert_alpha(),
 			groups = self.all_sprites,
 			z = LAYERS['ground'])
+
+	def player_add(self,item):
+
+		self.player.item_inventory[item] += 1
+
+	def reset(self):
+
+		# apples on the trees
+		for tree in self.tree_sprites.sprites():
+			for apple in tree.apple_sprites.sprites():
+				apple.kill()
+			tree.create_fruit()
 
 	def run(self,dt):
 		self.display_surface.fill('black')
@@ -73,6 +102,9 @@ class Level:
 		self.all_sprites.update(dt)
 
 		self.overlay.display()
+
+		if self.player.sleep:
+			self.transition.play()
 
 class CameraGroup(pygame.sprite.Group):
 	def __init__(self):
